2da7b1de4b833d1ed6add7efc9f4ec3d2bfb5674
monkeyQK <155392114@qq.com>
2018-09-26 08:21:53 +0800

    hello-flask

--- hello_flask.py
+++ hello_flask.py
@@ -1,2 +1,25 @@
-# my_note
-账号记录app
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+# 装饰器的作用url与视图函数的映射
+
+
+@app.route("/")
+def hello_flask():
+    return "hello flask!"
+
+
+@app.route("/login")
+def login_app():
+    return render_template("login.html", username="monkeyQK")
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
