c4654f04e9854271b1bbc9cba57bfc49bc84f600
monkeyQK <155392114@qq.com>
2018-09-28 12:44:24 +0800

    模板使用

--- hello_flask.py
+++ hello_flask.py
@@ -5,6 +5,22 @@ from database import app
 
 # 装饰器的作用url与视图函数的映射
 
+note_user = {
+    'username': 'monkeyQK',
+    'qq': '155392114'
+}
+
+
+posts = [  # fake array of posts
+    {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'nickname': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }]
+
 
 @app.route("/")
 def hello_flask():
@@ -13,7 +29,25 @@ def hello_flask():
 
 @app.route("/login")
 def login_app():
-    return render_template("login.html", username="monkeyQK")
+    page_name = "登录"
+    return render_template("login.html", page_name=page_name)
+
+
+@app.route("/regist")
+def reg_app():
+    page_name = "注册"
+    return render_template("reg.html", page_name=page_name)
+
+
+@app.route("/index")
+def index():
+    page_name = "首页"
+    return render_template("index.html", page_name=page_name)
+
+
+@app.route("/list")
+def show():
+    return render_template("list.html", posts=posts)
 
 
 @app.route("/init")
